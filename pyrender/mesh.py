246c670587d2c53cb37d0772aa9b7f2c9faa5639
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-05-21 23:15:11 +0800

    Replace np.infty and np.-infty with np.inf and np.-inf in Mesh class bounds calculation

--- pyrender/mesh.py
+++ pyrender/mesh.py
@@ -84,8 +84,8 @@ class Mesh(object):
         """(2,3) float : The axis-aligned bounds of the mesh.
         """
         if self._bounds is None:
-            bounds = np.array([[np.infty, np.infty, np.infty],
-                               [-np.infty, -np.infty, -np.infty]])
+            bounds = np.array([[np.inf, np.inf, np.inf],
+                               [-np.inf, -np.inf, -np.inf]])
             for p in self.primitives:
                 bounds[0] = np.minimum(bounds[0], p.bounds[0])
                 bounds[1] = np.maximum(bounds[1], p.bounds[1])
