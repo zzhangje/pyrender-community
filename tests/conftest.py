1503b0ff26e4bb1ca765c593558c6bdde268dcab
ZhangzrJerry <ZhangzrJerry@outlook.com>
2025-05-22 21:02:45 +0800

    Import Display module conditionally for CI environment in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import os
-from pyvirtualdisplay import Display
 
 # Start virtual display only in CI
 if os.getenv("CI"):
+    from pyvirtualdisplay import Display
     display = Display(visible=0, size=(1024, 768))
     display.start()
